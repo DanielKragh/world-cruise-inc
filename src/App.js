d503575bb9330d4780d86267254875b1ce307d7a
hathex <50103127+hathex@users.noreply.github.com>
2019-10-29 13:48:22 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // bootstrap 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import OpretKonto from './components/OpretKonto/OpretKonto';
+import TilmeldButik from './components/TilmeldButik/TilmeldButik';
 function App() {
   return (
     <div className="App">
-      <OpretKonto />
+      {/* <OpretKonto /> */}
+      <TilmeldButik/>
     </div>
   );
 }
