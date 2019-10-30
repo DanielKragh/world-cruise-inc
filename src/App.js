c7b187bcd915aefdc6fa180085279414e6e8e2a7
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-10-30 14:13:05 +0100

    login

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import "../node_modules/bootstrap/dist/js/bootstrap.js";
 import ButikAdmin from './components/ButikAdmin/ButikAdmin';
 import BrugerAdmin from './components/BrugerAdmin/BrugerAdmin';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Header/>
       <ButikAdmin />
       <BrugerAdmin />
+      <Footer />
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,45 +2,65 @@ import React from 'react';
 import { ReactComponent as Logo } from "./CruiseWorldInc_blaafarver.svg"
 // scss
 import "./Navigation.scss";
+import BrugerLogin from '../BrugerLogin';
 
 const Navigation = () => {
     return (
         <div className="position-fixed w-100 zindex">
             <nav role="navigation" id="navigation" className="row position-relative">
-            <a href="index.html" className="col-11">
-                <Logo className="logo" />
-            </a>
-            <div id="menuToggle" className="col-1">
+                <a href="index.html" className="col-11">
+                    <Logo className="logo" />
+                </a>
+                <div id="menuToggle" className="col-1">
 
-                <input type="checkbox" className="burger" />
+                    <input type="checkbox" className="burger" />
 
-                <span></span>
-                <span></span>
-                <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
 
 
 
-                <ul id="menu">
-                    <a href="#"><li>Forside</li></a>
-                    <a href="#"><li>Nyheder</li></a>
-                    <a href="#"><li>Kundeservice</li></a>
-                    <a href="#"><li>Firmaprofil</li></a>
-                    <a href="#"><li>Opret konto</li></a>
-                    <a href="#"><li>Tilmeld butik</li></a>
-                    <form action="">
-                        <input type="text" />
-                        <button type="submit">Search</button>
-                    </form>
-                    <div className="login">
-                        
+                    <ul id="menu">
+                        <a href="#"><li>Forside</li></a>
+                        <a href="#"><li>Nyheder</li></a>
+                        <a href="#"><li>Kundeservice</li></a>
+                        <a href="#"><li>Firmaprofil</li></a>
+                        <a href="#"><li>Opret konto</li></a>
+                        <a href="#"><li>Tilmeld butik</li></a>
+                        <form action="">
+                            <input type="text" />
+                            <button type="submit">Search</button>
+                        </form>
+                        <div className="login">
+                            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#BrugerModal">
+                                Login
+                            </button>
+                        </div>
+                    </ul>
+                </div>
+            </nav>
+            <div className="modal fade" id="BrugerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Bruger Login</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <BrugerLogin />
+                                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#ButikModal">
+                                Butik Login
+                            </button>                            <button type="button" className="btn btn-primary">Login Admin</button>
+                        </div>
                     </div>
-                </ul>
-
-
+                </div>
             </div>
-        </nav>
         </div>
-        
     )
 }
 
